08a0ae14e892daab523e68145f6810eab7f4e116
zhangsan <zhangsan@itcast.cn>
2018-12-26 18:46:30 +0800

    向本地仓库添加java工程

---
+++ git-test/src/com/itheima/FristClass.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class FristClass {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+        System.out.println("bbbbb");
+        System.out.println("ccccc");
+        System.out.println("ddddd");
+        System.out.println("eeeee");
+    }
+}
